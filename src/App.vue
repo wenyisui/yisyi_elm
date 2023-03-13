7be16f5d94219f83ab8946c50e21dd4514e922cb
wenbin <1969138070@qq.com>
2023-03-14 00:26:01 +0800

    upd

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,63 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div class="content">
+    <el-select v-model="chooseValue" filterable v-el-select-loadmore="loadMore(rangeNumber)"
+               multiple
+               collapse-tags
+    >
+      <el-option
+        v-for="(item, index) in options.slice(0, rangeNumber)"
+        :key="index"
+        :label="item.label"
+        :value="item.value">
+      </el-option>
+    </el-select>
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+ 
+<script>
+  export default {
+  data() {
+    return {
+      chooseValue: "",
+      options: [],
+      rangeNumber: 10,
+    };
+  },
+  methods: { 
+    getList() {
+      // 测试数据25000条数据, 这里数据多少条无所谓,options.slice(0, rangeNumber)方法只会默认加载初始的10条数据
+      for (let i = 0; i < 25000; i++) {
+        this.options.push({label: "选择"+i,value:"选择"+i});
+      } 
+    },
+    loadMore(n) {
+      // n是默认初始展示的条数会在渲染的时候就可以获取,具体可以打log查看
+      // elementui下拉超过7条才会出滚动条,如果初始不出滚动条无法触发loadMore方法
+      return () => (this.rangeNumber += 5); // 每次滚动到底部可以新增条数  可自定义
+    },
+  },
+  beforeMount() {
+    this.getList();
+  },
+  directives:{
+    'el-select-loadmore':(el, binding) => {
+      // 获取element-ui定义好的scroll盒子
+      const SELECTWRAP_DOM = el.querySelector(".el-select-dropdown .el-select-dropdown__wrap");
+      if(SELECTWRAP_DOM){
+        SELECTWRAP_DOM.addEventListener("scroll", function () {
+          /**
+           * scrollHeight 获取元素内容高度(只读)
+           * scrollTop 获取或者设置元素的偏移值,
+           *  常用于:计算滚动条的位置, 当一个元素的容器没有产生垂直方向的滚动条, 那它的scrollTop的值默认为0.
+           * clientHeight 读取元素的可见高度(只读)
+           * 如果元素滚动到底, 下面等式返回true, 没有则返回false:
+           * ele.scrollHeight - ele.scrollTop === ele.clientHeight;
+           */
+          const condition = this.scrollHeight - this.scrollTop -1 <= this.clientHeight;
+          if (condition) binding.value();
+        });
+      }
+    },
   }
 }
-</style>
+</script>
